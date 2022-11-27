278ab89694915ca68fb3f5e2da3232d6da604cc7
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-11-27 16:42:34 +0200

    installed react-icons library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+// import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
   return <h2>slider project setup</h2>;
 }
